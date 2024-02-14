2e99dc4524bf4eabfdc90a63beabafb72b2b61e6
PedroAraozC <pedroaraozc@gmail.com>
2024-02-14 18:59:23 -0300

    falta lograr que guarde el archivo en la carpeta

--- middlewares/multerStorage.js
+++ middlewares/multerStorage.js
@@ -1,26 +1,18 @@
-const multer = require("multer");
-const path = require('path');
-const fs = require('fs');
+// const express = require('express');
+// const router = express.Router();
+const multer = require('multer');
+// const { agregarBoletin } = require('../controllers/boletinController');
 
-const funcionMulter = (user) => {
-  
-    const storage = multer.diskStorage({
-      destination: function (req, file, cb) {
-        // cb(null, `./uploads/${user.nombreUsuario}`);
-        const uploadPath = `./uploads/${user.nombreUsuario}`; // Ruta de la carpeta de destino
-        fs.mkdirSync(uploadPath, { recursive: true }); // Crear carpeta si no existe
-        cb(null, uploadPath);
-      },
-      filename: function (req, file, cb) {
-        cb(null, Date.now() + "-" + file.originalname);
-      },
-    });
+// Configuración de multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Ruta donde se guardarán los archivos
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname); // Nombre de archivo único
+  }
+});
+const upload = multer({ storage: storage });
 
-    const upload = multer({ storage: storage });
 
-    return upload;
-};
-
-module.exports = {
-  funcionMulter,
-};
+module.exports = upload;
