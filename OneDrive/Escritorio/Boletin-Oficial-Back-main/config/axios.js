f792477e203b7669f23254398b66789498595e6f
Gabriela Mansilla <115327297+GabrielaMansilla@users.noreply.github.com>
2024-02-15 00:17:48 -0300

    Delete OneDrive/Escritorio directory

--- OneDrive/Escritorio/Boletin-Oficial-Back-main/config/axios.js
+++
@@ -1,9 +0,0 @@
-import axiosOriginal from "axios";
-
-const axios = axiosOriginal.create({
-  baseURL: "http://localhost:4000",
-//   baseURL: "http://10.0.0.230:4000",
-});
-
-
-export default axios;
