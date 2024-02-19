0f3b9e0314b95d0f4019b28601e2a562ce95aef9
PedroAraozC <pedroaraozc@gmail.com>
2024-02-19 17:02:10 -0300

    alta lista, falta descarga de archivo

--- controllers/boletinesControllers.js
+++ controllers/boletinesControllers.js
@@ -3,23 +3,33 @@ const CustomError = require("../utils/customError");
 const path = require("path");
 const ArchivoBoletin = require("../models//archivoBoletin");
 const multer = require("multer");
-const {funcionMulter} = require("../middlewares/multerStorage")
+const { funcionMulter } = require("../middlewares/multerStorage");
 const fs = require("fs");
+const archivoBoletin = require("../models//archivoBoletin");
 
 const agregarBoletin = async (req, res) => {
   try {
     funcionMulter()(req, res, async (err) => {
-       
       if (!req.file) {
         throw new CustomError("Error al cargar el archivo", 400);
       }
 
-      console.log(req.body,"19");
-      console.log(req.file, "20");
-      
-      const requestData = JSON.parse(req.body.requestData[1]);
-      
-      console.log(JSON.parse(req.body.requestData[1]),"24")
+      console.log(req.body, "16");
+      console.log(req.file, "17");
+
+      let requestData = "";
+
+      if (req.body.requestData === undefined) {
+        requestData = JSON.parse(req.body.requestData[1]);
+        console.log("hola");
+      } else {
+        requestData = JSON.parse(req.body.requestData);
+        console.log("adios");
+      }
+
+      // console.log(values.JSON.parse(req.body.requestData[1]), "29");
+      console.log(req.body.requestData[1], "30");
+      console.log(requestData, "31");
 
       const {
         nroBoletin,
@@ -32,26 +42,30 @@ const agregarBoletin = async (req, res) => {
       const newBoletin = new Boletin({
         nroBoletin,
         fechaBoletin,
-         nroDecreto,
+        nroDecreto,
         nroOrdenanza,
         nroResolucion,
       });
 
-       const boletinGuardado = await newBoletin.save();
-       if (!req.file) {
+      const boletinGuardado = await newBoletin.save();
+      if (!req.file) {
         throw new CustomError("File not provided", 400);
       }
-      console.log(typeof( req.file.fieldname), "51");
+      console.log(typeof req.file.fieldname, "51");
 
-     const rutaArchivo = path.join(__dirname, "../archivoBoletin", req.file.fieldname);
+      const rutaArchivo = path.join(
+        __dirname,
+        "../archivoBoletin",
+        req.file.fieldname
+      );
 
-     const newArchivoBoletin = new ArchivoBoletin({
-       rutaArchivo,
-       archivoBoletin: boletinGuardado._id,
-     });
+      const newArchivoBoletin = new ArchivoBoletin({
+        rutaArchivo,
+        archivoBoletin: boletinGuardado._id,
+      });
 
-     await newArchivoBoletin.save();
-     
+      await newArchivoBoletin.save();
+
       res.status(200).json({ message: "Se agregó un nuevo Boletín con éxito" });
     });
   } catch (error) {
@@ -70,7 +84,51 @@ const getBoletin = async (req, res) => {
   }
 };
 
+const obtenerArchivosDeUnBoletin = async (req, res) => {
+  try {
+    const archivosBoletin = await archivoBoletin
+      .find({ boletin: req.params.id })
+      .populate("boletin");
+
+      console.log(archivosBoletin)
+
+    if (archivosBoletin.length > 1) {
+      const zip = archiver("zip");
+
+      res.attachment("archivosBoletin.zip");
+      zip.pipe(res);
+
+      archivosBoletin.forEach((archivo) => {
+        if (fs.existsSync(archivo.rutaArchivo)) {
+          const partesRuta = archivo.rutaArchivo.split("\\");
+          const nombreArchivo = partesRuta[partesRuta.length - 1];
+          zip.append(fs.createReadStream(archivo.rutaArchivo), {
+            name: nombreArchivo,
+          });
+        } else {
+          throw new CustomError("archivo no encontrado", 404);
+        }
+      });
+
+      zip.finalize();
+    } else if (archivosBoletin.length == 0) {
+      throw new CustomError("Boletin sin archivos", 404);
+    } else {
+      if (fs.existsSync(archivosBoletin[0].rutaArchivo)) {
+        res.sendFile(archivosBoletin[0].rutaArchivo);
+      } else {
+        throw new CustomError("archivo no encontrado", 404);
+      }
+    }
+  } catch (error) {
+    res
+      .status(error.code || 500)
+      .json({ message: error.message || "algo explotó :|" });
+  }
+};
+
 module.exports = {
   agregarBoletin,
   getBoletin,
+  obtenerArchivosDeUnBoletin,
 };
--- routes/boletinRoutes.js
+++ routes/boletinRoutes.js
@@ -1,13 +1,17 @@
 const { Router } = require("express");
-const {funcionMulter} = require ("../middlewares/multerStorage")
+const { funcionMulter } = require("../middlewares/multerStorage");
 const {
   agregarBoletin,
   getBoletin,
+  obtenerArchivosDeUnBoletin,
 } = require("../controllers/boletinesControllers");
 
 const router = Router();
 
-router.post("/alta",  agregarBoletin);
 router.get("/listar", getBoletin);
+router.get("/listarDescarga/:id?", obtenerArchivosDeUnBoletin);
+
+router.post("/alta", agregarBoletin);
 router.post("/subir-archivo", funcionMulter);
+
 module.exports = router;
