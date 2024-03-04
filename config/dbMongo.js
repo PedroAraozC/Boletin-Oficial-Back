3296423c14dd38ff88c66f8b2f272fc380df80a6
PedroAraozC <pedroaraozc@gmail.com>
2024-03-04 14:43:33 -0300

    //

--- config/dbMongo.js
+++ config/dbMongo.js
@@ -4,6 +4,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URI);
     console.log("MongoDB connected");
+    console.log("Larry The Bird connected");
   } catch (error) {
     console.log(error.message);
   }
