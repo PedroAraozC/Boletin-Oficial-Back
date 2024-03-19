1b1e8579bb562787e31b6ea208990318d82e2b5a
GabrielaMansilla <gmansilla.utn.frt@gmail.com>
2024-03-19 14:52:02 -0300

    tabla de editar normas

--- controllers/normasControllers.js
+++ controllers/normasControllers.js
@@ -17,6 +17,45 @@ const getNormas = async (req, res) => {
   }
 };
 
+const putNormas = async (req, res) => {
+  try {
+    const db = await conectarMySql();
+    console.log(req.body);
+    const { id_norma, tipo_norma, habilita } = req.body;
+    
+    // Log para verificar los valores de los parámetros
+    console.log('Valores de los parámetros:', id_norma, tipo_norma, habilita);
+    
+    // Ejecutar la consulta SQL
+    await db.query(
+      'UPDATE norma SET id_norma = ?, tipo_norma = ?, habilita = ? WHERE id_norma = ?',
+      [id_norma, tipo_norma, habilita, id_norma] // Aquí, se deben pasar los parámetros como una matriz plana
+    );
+    
+    // Respuesta exitosa
+    res.status(200).json({ message: 'Norma actualizada con éxito' });
+  } catch (error) {
+    console.error('Error al actualizar norma:', error);
+    res.status(500).json({ message: 'Error al actualizar Norma' });
+  }
+};
+
+
+const getNormasTodo = async (req, res) => {
+  try {
+    const db = await conectarMySql();
+    if (!db || !db.query) {
+      throw new CustomError("Database connection or query function is missing", 500);
+    }const [normas] = await db.query( "SELECT * FROM norma");
+    // console.log([normas])
+    res.json(normas);
+  } catch (error) {
+    console.error("Error al buscar norma:", error);
+    res.status(500).json({ message: "Error al buscar norma" });
+  }
+};
+
+
 // // Función para obtener todas las normas
 // Norma.getAllNormas = async () => {
 //   try {
@@ -80,4 +119,4 @@ const getNormas = async (req, res) => {
 //   }
 // };
 
-module.exports = { getNormas };
+module.exports = { getNormas, putNormas, getNormasTodo };
