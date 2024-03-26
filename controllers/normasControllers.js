a31e6e840219fe85f99c1535adab71ddac85b6dc
PedroAraozC <pedroaraozc@gmail.com>
2024-03-26 18:37:19 -0300

    listo tabla editar norma flata terminar tabla origen, unirlas en TablasEdicion y ver que se hace con el archivo en editar boletin

--- controllers/normasControllers.js
+++ controllers/normasControllers.js
@@ -1,17 +1,22 @@
 const CustomError = require("../utils/customError");
 const path = require("path");
 const { conectarMySql } = require("../config/dbMySql");
-// const db = conectarMySql();
 
 const getNormas = async (req, res) => {
   try {
     const db = await conectarMySql();
     if (!db || !db.query) {
-      throw new CustomError("Database connection or query function is missing", 500);
-    }const [normas] = await db.query( "SELECT * FROM norma WHERE habilita = 1");
-    // console.log([normas])
+      throw new CustomError(
+        "Database connection or query function is missing",
+        500
+      );
+    }
+    const [normas] = await db.query("SELECT * FROM norma WHERE habilita = 1");
+
     res.json(normas);
+    await db.end();
   } catch (error) {
+    await db.end();
     console.error("Error al buscar norma:", error);
     res.status(500).json({ message: "Error al buscar norma" });
   }
@@ -20,103 +25,65 @@ const getNormas = async (req, res) => {
 const putNormasListado = async (req, res) => {
   try {
     const db = await conectarMySql();
-    // console.log(req.body);
+
     const { id_norma, tipo_norma, habilita } = req.body;
-    
-    // Log para verificar los valores de los parámetros
-    // console.log('Valores de los parámetros:', id_norma, tipo_norma, habilita);
-    
-    // Ejecutar la consulta SQL
+
     await db.query(
-      'UPDATE norma_prueba SET id_norma = ?, tipo_norma = ?, habilita = ? WHERE id_norma = ?',
-      [id_norma, tipo_norma, habilita, id_norma] // Aquí, se deben pasar los parámetros como una matriz plana
+      "UPDATE norma_prueba SET id_norma = ?, tipo_norma = ?, habilita = ? WHERE id_norma = ?",
+      [id_norma, tipo_norma.toUpperCase(), habilita, id_norma] // Aquí, se deben pasar los parámetros como una matriz plana
     );
-    
-    // Respuesta exitosa
-    res.status(200).json({ message: 'Norma actualizada con éxito' });
+
+    res.status(200).json({ message: "Norma actualizada con éxito" });
+    await db.end();
   } catch (error) {
-    console.error('Error al actualizar norma:', error);
-    res.status(500).json({ message: 'Error al actualizar Norma' });
+    await db.end();
+    console.error("Error al actualizar norma:", error);
+    res.status(500).json({ message: "Error al actualizar Norma" });
   }
 };
-
 
 const getNormasListado = async (req, res) => {
   try {
     const db = await conectarMySql();
     if (!db || !db.query) {
-      throw new CustomError("Database connection or query function is missing", 500);
-    }const [normas] = await db.query( "SELECT * FROM norma_prueba");
-    // console.log([normas])
+      throw new CustomError(
+        "Database connection or query function is missing",
+        500
+      );
+    }
+    const [normas] = await db.query("SELECT * FROM norma_prueba");
     res.json(normas);
+    await db.end();
   } catch (error) {
+    await db.end();
     console.error("Error al buscar norma:", error);
     res.status(500).json({ message: "Error al buscar norma" });
   }
 };
 
+const postNorma = async (req, res) => {
+  try {
+    const db = await conectarMySql();
+    if (!db || !db.query) {
+      throw new CustomError(
+        "Database connection or query function is missing",
+        500
+      );
+    }
+    console.log(req.body);
 
-// // Función para obtener todas las normas
-// Norma.getAllNormas = async () => {
-//   try {
-//     const [rows] = await db.query("SELECT * FROM norma");
-//     return rows;
-//   } catch (error) {
-//     console.error("Error al obtener las normas:", error);
-//     throw error;
-//   }
-// };
+    const [result] = await db.query(
+      "INSERT INTO norma_prueba (tipo_norma, habilita) VALUES ( ?, ?)",
+      [req.body.norma.toUpperCase(), req.body.habilita]
+    );
 
-// // Función para obtener una norma por su ID
-// Norma.getNormaById = async (id) => {
-//   try {
-//     const [rows] = await db.query("SELECT * FROM norma WHERE id_norma = ?", [
-//       id,
-//     ]);
-//     return rows[0];
-//   } catch (error) {
-//     console.error("Error al obtener la norma por ID:", error);
-//     throw error;
-//   }
-// };
+    res.json(result);
+    await db.end();
+  } catch (error) {
+    await db.end();
+    console.error("Error al agregar norma:", error);
+    res.status(500).json({ message: "Error al agregar norma" });
+  }
+};
 
-// // Función para crear una nueva norma
-// Norma.createNorma = async (tipo_norma, habilita) => {
-//   try {
-//     const [result] = await db.query(
-//       "INSERT INTO norma (tipo_norma, habilita) VALUES (?, ?)",
-//       [tipo_norma, habilita]
-//     );
-//     return result.insertId;
-//   } catch (error) {
-//     console.error("Error al crear una nueva norma:", error);
-//     throw error;
-//   }
-// };
-
-// // Función para actualizar una norma existente
-// Norma.updateNorma = async (id, tipo_norma, habilita) => {
-//   try {
-//     await db.query(
-//       "UPDATE norma SET tipo_norma = ?, habilita = ? WHERE id_norma = ?",
-//       [tipo_norma, habilita, id]
-//     );
-//     return true;
-//   } catch (error) {
-//     console.error("Error al actualizar la norma:", error);
-//     throw error;
-//   }
-// };
-
-// // Función para eliminar una norma
-// Norma.deleteNorma = async (id) => {
-//   try {
-//     await db.query("DELETE FROM norma WHERE id_norma = ?", [id]);
-//     return true;
-//   } catch (error) {
-//     console.error("Error al eliminar la norma:", error);
-//     throw error;
-//   }
-// };
-
-module.exports = { getNormas, putNormasListado, getNormasListado };
+module.exports = { getNormas, putNormasListado, getNormasListado, postNorma };
